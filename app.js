cb857022e1a95317175a2533916de85d6e0d0ae9
unknown <ogunsanyaadewumi01@gmail.com>
2023-03-29 14:25:34 +0100

    Updating website

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ burger.addEventListener('click', ()=>{
    navlink.classList.toggle('active')
    burger.classList.toggle('change')
 })
+
+// Skill Progress Bar
+
+var spans = document.querySelectorAll('.progress-bar span')
+spans.forEach(span =>{
+   span.style.width = span.dataset.width
+   span.innerHTML = span.dataset.width
+})
+
